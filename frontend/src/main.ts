ed0fd0eb0952ee171483bfc601a028891f4713ec
nastassia martin <nastassia.martin@googlemail.com>
2022-12-28 22:57:32 +0100

    added quantity to each card

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -35,7 +35,8 @@ const renderProducts = () => {
            <div data-id="${product.id}" class="card-body">
               <h3 data-id="${product.id}" class="card-title pt-3">${product.name}</h3>
               <p data-id="${product.id}" class="card-text">${product.price} kr</p>
-              <button class = "clr-button" data-id="${product.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              <p data-id="${product.id}" class="card-text">${product.stock_quantity} i lager</p>
+              <button id="test-disable" class = "clr-button" data-id="${product.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
               class="bi bi-basket" viewBox="0 0 16 16">
               <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
               </svg>Lägg till</button>
@@ -74,7 +75,25 @@ document.querySelector('.product-overview')!.innerHTML = `<div class="col-6">
 document.querySelector('.filter-button')?.addEventListener('click', () => {
   renderProducts()
 })
+/*
 
+tried to disable button if stock was null, no chance! 
+products.filter ( (product) => {
+  if (product.stock_quantity <= 1){
+    document.querySelector('.clr-button')!.setAttribute('disabled', 'disabled')
+  }
+})
+
+
+
+// const OutOfStock = products.filter(( product => product.stock_quantity <1)) 
+//   console.log(OutOfStock) // logs all 10 items which are out of stock! how to access this?????
+
+// for(let i = 0; products[i].stock_quantity < 0 ; i ++) 
+// {
+// console.log(products[i].stock_quantity)
+// }
+*/
 
 
 }
