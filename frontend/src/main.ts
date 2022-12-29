5de86be8929beeff8a72a356ffa51e01f06122d1
Astrid Lindfors <astrid.lindfors@gmail.com>
2022-12-29 22:24:04 +0100

    added populateOrder file. with calculation for rendering total sum of chipping cart. Currently not rendering the whole shopping cart -just the order of the complete amount from one product

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,32 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import './styles/style.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/style.css";
 import { fetchData } from "./API";
 import { IProduct, IOrderInfo, IOrder, ICartItems } from "./interfaces";
 import { Modal, Offcanvas } from "bootstrap";
+import { populateOrder } from "./populateOrder";
 
 //(E2S1T3) - add when we are doing E3
 //import { addToCart } from './btn-click-counter-trolley'
 //addToCart() */
 
-const rowEl = document.querySelector('.row')
-const URL = 'https://www.bortakvall.se/'
+const rowEl = document.querySelector(".row");
+const URL = "https://www.bortakvall.se/";
 
 // Empty array to fetch data to
-let products: IProduct[] = []
+let products: IProduct[] = [];
 
 // Get data from API and save it into products-array
 const getProducts = async () => {
-  const result = await fetchData()
-  products = result.data
+  const result = await fetchData();
+  products = result.data;
   renderProducts();
-}
+};
 
-getProducts()
+getProducts();
 
 // Render products to DOM
 const renderProducts = () => {
   rowEl!.innerHTML = products
-    .map(product => `<div class="col-6 col-sm-4 col-lg-3">
+    .map(
+      (product) => `<div class="col-6 col-sm-4 col-lg-3">
          <div data-id="${product.id}" class="card mt-5">
           <img data-id="${product.id}" class="card-img img-fluid" src="${URL}${product.images.large}" alt="Image of ***">
            <div data-id="${product.id}" class="card-body">
@@ -40,34 +42,31 @@ const renderProducts = () => {
          </div>
        </div>
        `
-    ).join('')
-}
-
+    )
+    .join("");
+};
 
 // create variables to use modal (bootstrap)
-const modalEl = document.getElementById('moreInfoModal')!
-const modal = new Modal(modalEl)
+const modalEl = document.getElementById("moreInfoModal")!;
+const modal = new Modal(modalEl);
 
-// should be able to be different depending on what eventListener used 
+// should be able to be different depending on what eventListener used
 let clickedItem: any;
 let index: any;
 
 // function to find the index of the item clicked
 const findIndex = () => {
   // save ID on clicked item to search for index
-  const clickedID = clickedItem.dataset.id
+  const clickedID = clickedItem.dataset.id;
   // search for index to get the rest of key values
-  index = products.findIndex(product => product.id === Number(clickedID))
-}
+  index = products.findIndex((product) => product.id === Number(clickedID));
+};
 
-
-
-// empty array to put cartItems in 
-let cartItems: ICartItems[] = []
+// empty array to put cartItems in
+let cartItems: ICartItems[] = [];
 
 // function to push clicked item to the cartItems array
 const addToCart = () => {
-
   // put a new item in cart with qty 1, if there is no items added already
   if (!cartItems[0]) {
     cartItems.unshift({
@@ -76,21 +75,22 @@ const addToCart = () => {
       image: products[index].images.thumbnail,
       qty: 0,
       item_price: products[index].price,
-      item_total: products[index].price
-    })
+      item_total: products[index].price,
+    });
   }
 
   // look for a product already in cart with the same id as the one being added
-  let doubleID = cartItems.find(item => item.id === products[index].id)
+  let doubleID = cartItems.find((item) => item.id === products[index].id);
 
   // find the index of the cart item that has the same id as the one being added,
   // instead of adding a new one, only add qty of that item and count item_total
   if (doubleID) {
-    const index = cartItems.findIndex(product => product.id === doubleID?.id)
-    cartItems[index].qty++
-    cartItems[index].item_total = cartItems[index].qty * cartItems[index].item_price
+    const index = cartItems.findIndex((product) => product.id === doubleID?.id);
+    cartItems[index].qty++;
+    cartItems[index].item_total =
+      cartItems[index].qty * cartItems[index].item_price;
   }
-  // if no dublicates was found, put the item in cart with qty 1 
+  // if no dublicates was found, put the item in cart with qty 1
   else {
     cartItems.unshift({
       id: products[index].id,
@@ -98,13 +98,16 @@ const addToCart = () => {
       image: products[index].images.thumbnail,
       qty: 1,
       item_price: products[index].price,
-      item_total: products[index].price
-    })
+      item_total: products[index].price,
+    });
   }
 
+  const order = populateOrder(cartItems);
+
   // print out added items to cart
-  document.querySelector('.offcanvas-body')!.innerHTML = cartItems
-    .map(cartItem => `
+  document.querySelector(".offcanvas-body")!.innerHTML = cartItems
+    .map(
+      (cartItem) => `
     <div class="container cart-item">
           <div class="cart-img col-2">
             <img src="${URL}${cartItem.image}" alt="">
@@ -115,11 +118,13 @@ const addToCart = () => {
           </div>
         </div>
     `
-    ).join('')
+    )
+    .join("");
 
   // print out added items to cart
-  document.querySelector('.offcanvas-body')!.innerHTML = cartItems
-    .map(cartItem => `
+  document.querySelector(".offcanvas-body")!.innerHTML = cartItems
+    .map(
+      (cartItem) => `
     <div class="container cart-item">
           <div class="cart-img col-2">
             <img src="${URL}${cartItem.image}" alt="">
@@ -149,51 +154,90 @@ const addToCart = () => {
           </div>
         </div>
     `
-    ).join('')
+    )
+    .join("");
 
   // print out 'fortsätt handla' and 'betala' buttons to cart
-  document.querySelector('.offcanvas-body')!.innerHTML += `
+  document.querySelector(".offcanvas-body")!.innerHTML += `
   <div class="button-container">
   <button type="button" class="clr-button" data-bs-dismiss="offcanvas" aria-label="Close">Fortsätt handla</button>
   <button type="button" class="clr-button">Betala</button>
   </div>
-  `
+  <div class ="total_order_container">
+  <h4>TOTALSUMMAN ${order.order_total}<h4>
+  <p>Varav moms<p>
+  <h4><h4> 
+  </div> 
+  `;
 
   // change nr of products in cart
-  const minusBtn = document.querySelector('.minus')
-  const plusBtn = document.querySelector('.plus')
-  const trashBtn = document.querySelector('.trash')
+  const minusBtn = document.querySelector(".minus");
+  const plusBtn = document.querySelector(".plus");
+  const trashBtn = document.querySelector(".trash");
 
-  minusBtn?.addEventListener('click', () => {
-    console.log('HEJ')
-  })
+  minusBtn?.addEventListener("click", () => {
+    console.log("HEJ");
+  });
 
-  plusBtn?.addEventListener('click', () => {
-    console.log('HEJ')
-  })
+  plusBtn?.addEventListener("click", () => {
+    console.log("HEJ");
+  });
 
-  trashBtn?.addEventListener('click', () => {
-  })
+  trashBtn?.addEventListener("click", () => {});
+};
+// Why do we have this?
+/* cartItems.push({
+  id: 0,
+  name: '',
+  image: 'tj',
+  qty: 0,
+  item_price: 4,
+  item_total: 1,
+})
+ */
 
-}
+console.log("cart items", cartItems);
+let orderInfo: IOrderInfo[] = cartItems.map((item) => {
+  return {
+    product_id: item.id,
+    qty: item.qty,
+    item_price: item.item_price,
+    item_total: item.item_total, // totala summan för hela mängden av en produkt
+  };
+});
 
-rowEl?.addEventListener('click', e => {
+//cartItems.filter(orderInfo[].item_price)
+let order: IOrder = {
+  customer_first_name: "",
+  customer_last_name: "",
+  customer_address: "",
+  customer_postcode: 0,
+  customer_city: "",
+  customer_email: "",
+  order_total: 0, // summan för allt
+  order_items: orderInfo,
+};
+console.log("order", order);
 
+rowEl?.addEventListener("click", (e) => {
   // save e.target to clickedItem
-  clickedItem = e.target as HTMLElement
+  clickedItem = e.target as HTMLElement;
 
   // if click on picture, card, name or price
   if (clickedItem.className !== "clr-button") {
     // call function to find index of products to print
-    findIndex()
+    findIndex();
     // open modal
-    modal.show()
+    modal.show();
 
     // print out headline to modal section
-    document.querySelector('.heading-container')!.innerHTML = `<h2>${products[index].name}</h2>`
+
+    document.querySelector(
+      ".heading-container"
+    )!.innerHTML = `<h2>${products[index].name}</h2>`;
 
     // print modal to DOM
-    document.querySelector('.modal-body')!.innerHTML = `
+    document.querySelector(".modal-body")!.innerHTML = `
     <div class="container">
       <div class="row">        
         <div class="col-6">
@@ -212,18 +256,16 @@ rowEl?.addEventListener('click', e => {
         </div>
       </div>
     </div>
-        `
+        `;
     // add button to cart through modal 'lägg till' button
-    document.querySelector('.modal-button')?.addEventListener('click', () => {
-      findIndex()
-      addToCart()
-    })
+    document.querySelector(".modal-button")?.addEventListener("click", () => {
+      findIndex();
+      addToCart();
+    });
   }
   // add item to cart through card 'lägg till'-button
   else if (clickedItem.className === "clr-button") {
-    findIndex()
-    addToCart()
+    findIndex();
+    addToCart();
   }
-})
-
-
+});
---
+++ frontend/src/populateOrder.ts
@@ -0,0 +1,35 @@
+import { ICartItems, IOrder, IOrderInfo } from "./interfaces";
+
+const calculateOrderTotal = (orderInfoList: IOrderInfo[]) => {
+  // return orderTotal.
+  // orderTotal s the quantity of each product * price + sum of each unique product
+  let orderSum = 0;
+  orderInfoList.forEach((item) => {
+    orderSum = item.item_price * item.qty;
+  });
+  return orderSum;
+};
+
+export const populateOrder = (cartItems: ICartItems[]) => {
+  let orderInfo: IOrderInfo[] = cartItems.map((item) => {
+    return {
+      product_id: item.id,
+      qty: item.qty,
+      item_price: item.item_price,
+      item_total: item.item_total, // totala summan för hela mängden av en produkt
+    };
+  });
+
+  //cartItems.filter(orderInfo[].item_price)
+  let order: IOrder = {
+    customer_first_name: "",
+    customer_last_name: "",
+    customer_address: "",
+    customer_postcode: 0,
+    customer_city: "",
+    customer_email: "",
+    order_total: calculateOrderTotal(orderInfo), // summan för allt
+    order_items: orderInfo,
+  };
+  return order;
+};
