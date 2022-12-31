fa5e0b6ac52b5ce3c6af7d56aaa1f333f3d18313
Hanna Björling <hanna.bjorling@hotmail.com>
2022-12-31 08:27:58 +0100

    fixed issue where lägg-till-btn wasnt disabled if outofstock

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -46,7 +46,7 @@ const renderProducts = () => {
               <h3 data-id="${product.id}" class="card-title pt-3">${product.name}</h3>
               <p data-id="${product.id}" class="card-text">${product.price} kr</p>
               <p data-id="${product.id}" class="card-text stock-qty">${product.stock_quantity} i lager</p>
-              <button id="product-num${product.id}" class = "clr-button" data-id="${product.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              <button id="product-num${product.id}" class ="clr-button" data-id="${product.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
               class="bi bi-basket" viewBox="0 0 16 16">
               <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
               </svg>Lägg till</button>
@@ -60,11 +60,12 @@ const renderProducts = () => {
   //Test 1 THIS WORKS
   const disableButton = () => {
     products.forEach((product) => {
-      if (product.stock_quantity < 1) {
+      if (product.stock_status === "outofstock") {
         document
           .querySelector(`#product-num${product.id}`)!
           .setAttribute("disabled", "disabled");
       }
+      console.log(product)
     });
   };
 
@@ -91,7 +92,7 @@ const renderProducts = () => {
 <p>Varav ${inStockProducts.length} i lager</p>
 </div>
 <div class="col-6 filter">
-<button type="button" class ="filter-button button">Filtrera (A-Ö)</button>
+<button type="button" class="filter-button button">Filtrera (A-Ö)</button>
 </div>
 `;
   document.querySelector(".filter-button")?.addEventListener("click", () => {
@@ -237,7 +238,7 @@ rowEl?.addEventListener("click", (e) => {
     </div>
         `;
     // if product is out of stock, disable 'lägg till'-btn 
-    if (products[index].stock_quantity < 1) {
+    if (products[index].stock_status === "outofstock") {
       document
         .querySelector(`#product-num${products[index].id}`)!
         .setAttribute("disabled", "disabled");
