c079c71fadff0531dd62411c0a5880eb4f1d0be2
Hanna Björling <hanna.bjorling@hotmail.com>
2023-02-15 10:50:18 +0100

    removed lägg-till-btn in modal

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -49,6 +49,8 @@ const getProducts = async () => {
 
 getProducts()
 
+console.log(products)
+
 // 2. For each product not in stock, set value from null to 0
 const checkStockStatus = () => {
   products.forEach((product) => {
@@ -113,6 +115,8 @@ const renderProducts = () => {
     </div>`
     )
     .join("")
+
+
 
   // 4. If product is out of stock - set 'lägg till'-btn to disabled
   products.forEach((product) => {
@@ -294,10 +298,6 @@ rowEl?.addEventListener('click', e => {
           <p class="text-center">Artikel nr: ${products[index].id}</p>
           ${products[index].description}
           <p class="modal-price text-center">${products[index].price} kr</p>
-          <button id="product-num${products[index].id}" class="text-center clr-button modal-button" tabindex="-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-          class="bi bi-basket" viewBox="0 0 16 16">
-          <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
-          </svg>Lägg till</button>
         </div>
       </div>
     </div>`
